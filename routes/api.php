1e5bd0951758cfd3e1034137f05e486379741d86
Kevin Chandra <kacol.bot@gmail.com>
2020-09-02 00:35:02 +0700

    employee resource

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')
+     ->name('api.')
+     ->group(function () {
+         Route::apiResource('employees', 'EmployeeController');
+     });
