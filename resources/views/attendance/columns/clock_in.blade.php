b17a1513104e303b10561e395b5bc7f2f8984e00
Kevin Chandra <kacol.bot@gmail.com>
2020-09-12 15:55:45 +0700

    fix make clockout empty if not defined

--- resources/views/attendance/columns/clock_in.blade.php
+++ resources/views/attendance/columns/clock_in.blade.php
@@ -1,7 +1,11 @@
-<span data-order="{{ $entry->start_at }}">
-	{{ \Carbon\Carbon::parse($entry->start_at)
-            ->locale(app()->getLocale())
-            ->format('d M Y, H:i:s') }}
+@php
+    $column['text'] = empty($entry->start_at) ? '' :
+            \Carbon\Carbon::parse($entry->start_at)
+                ->locale(App::getLocale())
+                ->format('d M Y, H:i:s');
+@endphp
+<span data-order="{{ $entry->start_at ?? '' }}">
+	{{ $column['text'] }}
     @if(empty($entry->selfie_in))
     @else
         <img src="{{ asset('storage/' . $entry->selfie_in) }}" alt="selfie in" style="max-height: none;width: 100px;"
--- resources/views/attendance/columns/clock_out.blade.php
+++ resources/views/attendance/columns/clock_out.blade.php
@@ -1,7 +1,11 @@
-<span data-order="{{ $entry->end_at }}">
-	{{ \Carbon\Carbon::parse($entry->end_at)
-            ->locale(app()->getLocale())
-            ->format('d M Y, H:i:s') }}
+@php
+    $column['text'] = empty($entry->end_at) ? '' :
+            \Carbon\Carbon::parse($entry->end_at)
+                ->locale(App::getLocale())
+                ->format('d M Y, H:i:s');
+@endphp
+<span data-order="{{ $entry->end_at ?? '' }}">
+	{{ $column['text'] }}
     @if(empty($entry->selfie_out))
     @else
         <img src="{{ asset('storage/' . $entry->selfie_out) }}" alt="selfie in" style="max-height: none;width: 100px;"
