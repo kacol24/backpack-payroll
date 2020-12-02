e143904fc99888150f08905d3c9598b8fb6c2d81
Kevin Chandra <kacol.bot@gmail.com>
2020-12-02 12:45:27 +0700

    improve overlay spinner

--- resources/views/reports/attendance/calendar.blade.php
+++ resources/views/reports/attendance/calendar.blade.php
@@ -6,12 +6,15 @@
     <style>
         .overlay {
             position: absolute;
-            background-color: rgba(255, 255, 255, .5);
+            background-color: rgba(255, 255, 255, .9);
             top: 0;
             left: 0;
             width: 100%;
             height: 100%;
             z-index: 999;
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
     </style>
 @endpush
@@ -48,34 +51,6 @@
         });
         calendar.render();
     </script>
-    {{--    <script>--}}
-    {{--        var calendarEl = document.getElementById('attendance_calendar');--}}
-    {{--        var calendar = new FullCalendar.Calendar(calendarEl, {--}}
-    {{--            initialView: 'timeGridWeek',--}}
-    {{--            headerToolbar: {--}}
-    {{--                left: 'prev,next today',--}}
-    {{--                center: 'title',--}}
-    {{--                right: 'timeGridDay,timeGridWeek,timeGridMonth'--}}
-    {{--            },--}}
-    {{--            views: {--}}
-    {{--                timeGridMonth: {--}}
-    {{--                    type: 'timeGrid',--}}
-    {{--                    duration: {month: 1},--}}
-    {{--                    buttonText: 'Month'--}}
-    {{--                }--}}
-    {{--            },--}}
-    {{--            allDaySlot: false,--}}
-    {{--            events: '{{ route('api.attendances.index') }}',--}}
-    {{--            loading: function(isLoading) {--}}
-    {{--                if (isLoading) {--}}
-    {{--                    $('.overlay').show();--}}
-    {{--                } else {--}}
-    {{--                    $('.overlay').hide();--}}
-    {{--                }--}}
-    {{--            }--}}
-    {{--        });--}}
-    {{--        calendar.render();--}}
-    {{--    </script>--}}
 @endpush
 
 @section('header')
@@ -92,15 +67,10 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">
-                        asdjkl
-                    </h4>
-                </div>
                 <div class="card-body position-relative">
                     <div id="attendance_calendar"></div>
                     <div class="overlay">
-                        <i class="la la-refresh la-spin la-fw"></i>
+                        <i class="la la-refresh la-spin la-fw la-2x la-spin"></i>
                     </div>
                 </div>
             </div>
