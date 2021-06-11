70dbe065ae886980fe2fe17f51dc85faf23eeb7d
Kevin Chandra <kacol.bot@gmail.com>
2021-06-11 11:58:59 +0700

    DRY allowance and deduction crud controller

--- app/Http/Controllers/Admin/AllowanceCrudController.php
+++ app/Http/Controllers/Admin/AllowanceCrudController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\AllowanceRequest;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Backpack\CRUD\app\Http\Controllers\Operations\ReorderOperation;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
@@ -13,15 +11,8 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
  * @package App\Http\Controllers\Admin
  * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
  */
-class AllowanceCrudController extends CrudController
+class AllowanceCrudController extends AllowanceDeductionController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
-    use ReorderOperation;
-
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
      *
@@ -44,33 +35,6 @@ class AllowanceCrudController extends CrudController
     }
 
     /**
-     * Define what happens when the List operation is loaded.
-     *
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     * @return void
-     */
-    protected function setupListOperation()
-    {
-        $this->crud->addColumn([
-            'name' => 'name',
-        ]);
-        $this->crud->addColumn([
-            'name' => 'description',
-        ]);
-        $this->crud->addColumn([
-            'name'  => 'is_active',
-            'type'  => 'check',
-            'label' => 'Active?',
-        ]);
-        $this->crud->addColumn([
-            'name'  => 'lft',
-            'label' => 'Order',
-        ]);
-
-        $this->crud->addClause('ordered');
-    }
-
-    /**
      * Define what happens when the Create operation is loaded.
      *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
@@ -80,27 +44,6 @@ class AllowanceCrudController extends CrudController
     {
         CRUD::setValidation(AllowanceRequest::class);
 
-        $this->crud->addField([
-            'name' => 'name',
-        ]);
-        $this->crud->addField([
-            'name' => 'description',
-        ]);
-        $this->crud->addField([
-            'name'  => 'is_active',
-            'type'  => 'checkbox',
-            'label' => 'Active?',
-        ]);
-    }
-
-    /**
-     * Define what happens when the Update operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
-     * @return void
-     */
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
+        parent::setupCreateOperation();
     }
 }
--- app/Http/Controllers/Admin/DeductionCrudController.php
+++ app/Http/Controllers/Admin/DeductionCrudController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\DeductionRequest;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Backpack\CRUD\app\Http\Controllers\Operations\ReorderOperation;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
 /**
@@ -13,15 +11,8 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
  * @package App\Http\Controllers\Admin
  * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
  */
-class DeductionCrudController extends CrudController
+class DeductionCrudController extends AllowanceDeductionController
 {
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
-    use ReorderOperation;
-
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
      *
@@ -34,42 +25,6 @@ class DeductionCrudController extends CrudController
         CRUD::setEntityNameStrings('deduction', 'deductions');
     }
 
-    protected function setupReorderOperation()
-    {
-        // define which model attribute will be shown on draggable elements
-        $this->crud->set('reorder.label', 'name');
-        // define how deep the admin is allowed to nest the items
-        // for infinite levels, set it to 0
-        $this->crud->set('reorder.max_level', 1);
-    }
-
-    /**
-     * Define what happens when the List operation is loaded.
-     *
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     * @return void
-     */
-    protected function setupListOperation()
-    {
-        $this->crud->addColumn([
-            'name' => 'name',
-        ]);
-        $this->crud->addColumn([
-            'name' => 'description',
-        ]);
-        $this->crud->addColumn([
-            'name'  => 'is_active',
-            'type'  => 'check',
-            'label' => 'Active?',
-        ]);
-        $this->crud->addColumn([
-            'name'  => 'lft',
-            'label' => 'Order',
-        ]);
-
-        $this->crud->addClause('ordered');
-    }
-
     /**
      * Define what happens when the Create operation is loaded.
      *
@@ -80,27 +35,6 @@ class DeductionCrudController extends CrudController
     {
         CRUD::setValidation(DeductionRequest::class);
 
-        $this->crud->addField([
-            'name' => 'name',
-        ]);
-        $this->crud->addField([
-            'name' => 'description',
-        ]);
-        $this->crud->addField([
-            'name'  => 'is_active',
-            'type'  => 'checkbox',
-            'label' => 'Active?',
-        ]);
-    }
-
-    /**
-     * Define what happens when the Update operation is loaded.
-     *
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
-     * @return void
-     */
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
+        parent::setupCreateOperation();
     }
 }
